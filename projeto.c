37f3c757c2f5469ada5c11224ccc77c33292b112
zezin <rodrigo.correa.bitt@gmail.com>
2024-05-03 11:50:06 -0300

    projeto-colocado

--- projeto.c
+++ projeto.c
@@ -1,72 +1,65 @@
 #include <stdio.h>
 #include <math.h>
 
-double soma(int n)
+void interface()
 {
-    int i=1; //contador do laco de repeticao
-    double numero, ultimo_numero; //numeros que serao somados
-    double soma=0; // valor da soma
+    printf(" .d8888b.        d8888 888      .d8888b.  888     888 888             d8888 888888888   .d88888b.  88888888          d8888 \n");
+    printf("c88P  Y88b      d88888 888     d88P  Y88b 888     888 888            d88888 888    88b d88P    88b 888   Y88b       d88888 \n");
+    printf("888    888     d88P888 888     888    888 888     888 888           d88P888 888    888 888     888 888    888      d88P888\n");
+    printf("888           d88P 888 888     888        888     888 888          d88P 888 888    888 888     888 888   d88P     d88P 888\n");
+    printf("888          d88P  888 888     888        888     888 888         d88P  888 888    888 888     888 88888888      d88P  888\n");
+    printf("888    888  d88P   888 888     888    888 888     888 888        d88P   888 888    888 888     888 888 T88b     d88P   888\n");
+    printf("Y88b  d88P d8888888888 888     Y88b  d88P Y88b. .d88P 888       d8888888888 888  .d88P Y88b. .d88P 888  T88b   d8888888888\n");
+    printf(" 8Y8888P  d88P     888 88888888  Y8888P     Y88888P   88888888 d88P     888 888888888   Y88888P    888   T88b d88P     888\n\n");
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+    printf("8888     d888 8888888 888           .d8888b.  888888888         d8888 888     888     888\n");                               
+    printf("88888   d8888   888   888          d88P  Y88b 888   Y88b       d88888 888     888     888\n");                            
+    printf("8888888888888   888   888          888    888 888    888      d88P888 888     888     888\n");                         
+    printf("8888888888888   888   888          888        888   d88P     d88P 888 888     888     888\n");                      
+    printf("888 Y888P 888   888   888          888  88888 88888888      d88P  888 888     888     888\n");                   
+    printf("888  Y8P  888   888   888          888    888 888 T88b     d88P   888 888     888     Y8P\n");                
+    printf("888       888   888   888          Y88b  d88P 888  T88b   d8888888888 Y88b. .d88P      | \n");             
+    printf("888       888 8888888 88888888      Y8888P88 888   T88b  d88P     888   Y88888P       888\n\n");          
 
-    printf("Insira a quantidade de numeros que serao somados abaixo:\n");
-    scanf("%d\n", &n);
+    printf("|---------------------------|\n");
+    printf("|*** Menu da calculadora ***|\n");
+    printf("|---------------------------|\n\n");
 
-    printf("Agora, coloque os numero a serem somados:\n");
+    printf("1 - Soma\n");
+    printf("2 - Subtracao\n");
+    printf("3 - Multiplicacao\n");
+    printf("4 - Divisao\n");
+    printf("5 - Potenciacao\n");
+    printf("6 - Raiz Quadrada\n");
+    printf("7 - Sair\n\n");
 
-    while (i<n)
-    {
-        scanf("%lf", &numero);
-        printf("+\n");
-
-        soma = soma + numero; // a soma comeca em 0, e acrescenta o numero a cada loop
-
-        i++; //o laco repete ate chegar na quantidade delimitada pelo usuario
-    }    
-
-    scanf("%lf", &ultimo_numero); //coloca-se separado o ultimo numero para nao sobrar um "+" no final
-    printf("---------------------------\n\n");
-
-    soma = soma + ultimo_numero;
-
-    printf("O resultado eh:\n %.2lf\n", soma);
-
-    return soma;
 }
-
-double subtracao()
-{
-    double n1;
-}
-
 
 
 int main()
 {
-    int escolha; //valor da escolha do menu
+    int operacao;
 
+    interface();
 
-    printf("***Menu de opcoes da calculadora:***\n\n");
+    printf("Digite o numero da operacao abaixo, ou '7' para sair\n");
+    scanf("%d\n", operacao);
 
-    printf("1 - Soma\n");
-    printf("2 - subtracao\n");
-    printf("3 - Multiplicacao\n");
-    printf("4 - Divisao\n");
-    printf("4 - Potenciacao\n");
-    printf("5 - Raiz Quadrada\n");
-    printf("6 - Sair\n\n");
-
-    printf("Escolha sua opcao abaixo, ou aperte 6 para encerrar o programa:\n");
-    scanf("%d\n", escolha);
-
-    switch (escolha)
+    if(operacao == 1)
     {
-        case 1:
-        int p;
         soma();
-        break;
-    
-        default:
-        break;
     }
-    
+
+    else if(operacao == 2)
+    {
+        subtracao();
+    }
+
+
+
+
+
     return 0;
 }
